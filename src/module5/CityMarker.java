6dbcf2a7636c03ad617a27d202ba515f73dd140d
dhruv3 <dhruv.mnit@gmail.com>
2017-01-20 23:43:59 -0500

    Implemented till Step 6

--- src/module5/CityMarker.java
+++ src/module5/CityMarker.java
@@ -16,7 +16,7 @@ import processing.core.PGraphics;
 // TODO: Change SimplePointMarker to CommonMarker as the very first thing you do 
 // in module 5 (i.e. CityMarker extends CommonMarker).  It will cause an error.
 // That's what's expected.
-public class CityMarker extends SimplePointMarker {
+public class CityMarker extends CommonMarker {
 	
 	public static int TRI_SIZE = 5;  // The size of the triangle marker
 	
@@ -50,8 +50,8 @@ public class CityMarker extends SimplePointMarker {
 	/** Show the title of the city if this marker is selected */
 	public void showTitle(PGraphics pg, float x, float y)
 	{
-		
 		// TODO: Implement this method
+		pg.text(getCity(), x, y);
 	}
 	
 	
@@ -71,5 +71,12 @@ public class CityMarker extends SimplePointMarker {
 	public float getPopulation()
 	{
 		return Float.parseFloat(getStringProperty("population"));
+	}
+
+
+	@Override
+	public void drawMarker(PGraphics pg, float x, float y) {
+		// TODO Auto-generated method stub
+		
 	}
 }
